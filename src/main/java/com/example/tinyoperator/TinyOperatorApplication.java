4141465e64c533524d880aaeb4ed5124ea15c871
Luis vcn <khanccer@gmail.com>
2023-02-04 00:54:06 -0500

    tiny v1

---
+++ src/main/java/com/example/tinyoperator/TinyOperatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.tinyoperator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyOperatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyOperatorApplication.class, args);
+	}
+
+}
